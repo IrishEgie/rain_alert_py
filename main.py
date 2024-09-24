08fe55827596af45ca7648d957f75e3f90c0c17f
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-24 14:51:02 +0800

    update: fetch specific data from json using list comprehension

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import requests as rq
 from config import *
 
-LAT = 11.589979
-LON = 124.986818
 
+weather_param = {
+    "lat": 11.589979,
+    "lon": 124.986818,
+    "appid": api_key,
+    "cnt": 4,
+}
+response = rq.get(f"https://api.openweathermap.org/data/2.5/forecast?", params=weather_param)
+response.raise_for_status()
 
-response = rq.get(f"api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={api_key}")
+weather_data = response.json()["list"]
+half_day_forecast = [hour["weather"][0]["id"] for hour in weather_data] #this is a new list
+for weather_code in half_day_forecast:
+
+    if int(weather_code) < 700:
+        print("Probable Rain detected, Please bring an umbrella")
+# print(half_day_forecast)
