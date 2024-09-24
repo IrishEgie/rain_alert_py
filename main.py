4f0245348055548176eaa8b434879dd02b02921f
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-24 17:49:07 +0800

    update: added email alert for the weather check

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests as rq
 from config import *
 from twilio.rest import Client
-
+from datetime import datetime as dt
+import smtplib
 
 def send_sms():
     client = Client(account_sid, auth_token)
@@ -19,6 +20,14 @@ weather_param = {
     "appid": api_key,
     "cnt": 4,
 }
+
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL,password=EMAIL_APP_PASS)
+        connection.sendmail(from_addr=EMAIL,to_addrs=send_to, msg="Subject: Weather Today\n\nHigh chances of rain today, don't forget to bring an umbrella")
+    print(f"Email sent to {send_to} on {dt.now()}")
+
 response = rq.get(f"https://api.openweathermap.org/data/2.5/forecast?", params=weather_param)
 response.raise_for_status()
 
@@ -28,6 +37,6 @@ half_day_forecast = [hour["weather"][0]["id"] for hour in weather_data] #this is
 for weather_code in half_day_forecast:
     if int(weather_code) < 700:
         print("Probable Rain detected, Please bring an umbrella⛱")
-        send_sms()
+        send_email()
         break
 # print(half_day_forecast)
