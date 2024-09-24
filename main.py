68fe68be8a707766644ec60eab7ed9c68b7565a9
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-24 15:22:32 +0800

    update: added a send sms function using twilio, didn't work as intended but the code logs on twilio which is still a success

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import requests as rq
 from config import *
+from twilio.rest import Client
 
+
+def send_sms():
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="High chances of rain today, don't forget to bring an umbrella 🌂",
+        from_="+17205752924",
+        to=my_num,
+    )
+    print(message.sid)
 
 weather_param = {
     "lat": 11.589979,
-    "lon": 124.986818,
+    "lon": 124.986818, 
     "appid": api_key,
     "cnt": 4,
 }
@@ -13,8 +24,10 @@ response.raise_for_status()
 
 weather_data = response.json()["list"]
 half_day_forecast = [hour["weather"][0]["id"] for hour in weather_data] #this is a new list
-for weather_code in half_day_forecast:
 
+for weather_code in half_day_forecast:
     if int(weather_code) < 700:
-        print("Probable Rain detected, Please bring an umbrella")
+        print("Probable Rain detected, Please bring an umbrella⛱")
+        send_sms()
+        break
 # print(half_day_forecast)
